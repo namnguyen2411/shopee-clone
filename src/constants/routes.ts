3ccb97bcaa11ab6bf940b57a3eaf489ba4e71fc4
NPN-PC <phuongnam.nguyen241196@gmail.com>
2024-01-20 00:08:09 +0700

    Install @floating-ui/react package, update profile route and purchases route

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,16 +1,18 @@
 const routes = {
+  // public routes
   home: '/',
   register: '/register',
   login: '/login',
   logout: '/logout',
   products: '/products',
   product: '/products/:id',
+  // private routes
+  profile: '/user/profile',
+  purchases: '/user/purchases',
   categories: '/categories',
-  purchases: '/purchases',
   addToCart: '/purchases/add-to-cart',
   updatePurchase: '/purchases/update-purchase',
   buyPurchase: '/purchases/buy-products',
-  getProfile: '/me',
   updateProfile: '/user',
   uploadAvatar: '/user/upload-avatar'
 } as const
